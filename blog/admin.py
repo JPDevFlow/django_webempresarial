259058c49fd82379dec49fde3f3836fd846cb8a0
JEHYP1021 <jehypo1021@gmail.com>
2025-06-02 11:54:38 +0200

    Se añade la personalizacion de blog en el panel de administracion para agregar entradas

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import category, Post
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields =('created', 'updated')
+
+admin.site.register(category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
