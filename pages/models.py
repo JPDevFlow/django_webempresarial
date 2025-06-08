1d825ed86c74660b6c28982d2812055567c00b05
JEHYP1021 <jehypo1021@gmail.com>
2025-06-08 12:14:01 +0200

    agregado en el panel de administracion la opcion de ordenar las paginas secundarias

--- pages/models.py
+++ pages/models.py
@@ -5,13 +5,14 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(verbose_name="Titulo", max_length=200)
     content = models.TextField(verbose_name="Contenido")
+    order = models.SmallIntegerField(verbose_name="Orden", default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de modificación")
 
     class Meta:
         verbose_name = "página"
         verbose_name_plural = "páginas"
-        ordering = ['title']
+        ordering = ['order', 'title']
     def __str__(self):
         return self.title
 
